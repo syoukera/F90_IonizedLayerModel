11d685cf17a365e842770c3697e9c4e1c4649e70
syoukera <s.akira2986@gmail.com>
2024-08-16 14:31:15 +0900

    [variables_module] update initialization

--- solve_electric_potential.f90
+++ solve_electric_potential.f90
@@ -9,31 +9,19 @@ program solve_electric_potential
 
     call initialize_variables()
 
-    ! ! 初期条件と境界条件の設定
-    ! V = 0.0d0
-    ! rho = 0.0d0
-
-    ! do i = 1, nx
-    !     rho(i) = max(exp(- pi*(x - H)/alpha**2), 0.0)
-    ! end do
-
-    ! ! 例として、中央に正負の電荷を配置
-    ! rho(nx/2) = 1.0d-6
-    ! rho(nx/2+1) = -1.0d-6
-
     ! SOR法でポアソン方程式を解く
     do k = 1, max_iter
         V_old = V
         do i = 2, nx-1
-        V(i) = (1.0d0 - omega) * V(i) + omega * 0.5d0 * &
-                (V(i+1) + V(i-1) + dx*dx*rho(i)/epsilon_0)
+            V(i) = (1.0d0 - omega) * V(i) + omega * 0.5d0 * &
+                    (V(i+1) + V(i-1) + dx*dx*rho(i)/epsilon_0)
         end do
 
         ! 収束チェック
         error = maxval(abs(V - V_old))
         if (error < tolerance) then
-        print *, 'Converged after ', k, ' iterations.'
-        exit
+            print *, 'Converged after ', k, ' iterations.'
+            exit
         end if
     end do
 
--- variables_module.f90
+++ variables_module.f90
@@ -1,19 +1,29 @@
 module variables_module
     implicit none
+
+    ! parameters for grid
     integer, parameter :: nx = 101
+    double precision, parameter :: dx        = 1.0d-2 ! distance between grid points [m]
+    double precision, parameter :: length_x  = (nx-1)*dx ! length of calclation domain [m]
+    
+    ! parameters for Gaussian profile
+    double precision, parameter :: height_flame = length_x/2.0 ! height of flame [m]
+    double precision, parameter :: a_thickness  = 1.0d-3 ! thickness parameter [m]
+    double precision, parameter :: epsilon_0    = 8.854187817d-12  ! vacuum permittivity [C/V m]
+    double precision, parameter :: q_e          = 1.602176634d-19  ! elementary charge [C]
+    double precision, parameter :: pi           = 3.141592653589d0
+    
+    ! parameters for computation
+    double precision, parameter :: tolerance = 1.0d-6
+    double precision, parameter :: omega     = 1.8d0 ! relaxation coefficient (1 < omega < 2)
+
+    ! set variables arrays
     double precision :: X(nx) ! position of eac grid point [m]
     double precision :: V(nx) ! electric potential [V]
     double precision :: n_p(nx) ! number density of positive ions
     double precision :: n_n(nx) ! number density of negative ions
     double precision :: n_e(nx) ! number density of electrons
     double precision :: rho(nx) ! density of electric charge
-
-    ! パラメータの設定
-    double precision, parameter :: dx        = 1.0d-2 ! x方向の格子間隔 [m]
-    double precision, parameter :: tolerance = 1.0d-6
-    double precision, parameter :: omega     = 1.8d0 ! 過緩和係数 (1 < omega < 2)
-    double precision, parameter :: epsilon_0 = 8.854187817d-12  ! vacuum permittivity [C/V m]
-    double precision, parameter :: q_e       = 1.602176634d-19  ! elementary charge [C]
 
     contains
 
@@ -22,13 +32,19 @@ module variables_module
         
         do i = 1, nx
             X(i) = (i-1) * dx
-            rho(i) = 0.0d0
-            ! rho(i) = max(exp(- pi*(x - H)/alpha**2), 0.0)
+            ! rho(i) = 0.0d0
+            rho(i) = 1.0d-6*max(exp(- pi*(X(i) - height_flame)**2/a_thickness**2), 0.0)
         end do
 
-        ! 例として、中央に正負の電荷を配置
-        rho(nx/2) = 1.0d-6
-        rho(nx/2+1) = -1.0d-6
+        ! set boundary on V
+        V(1) = 0.0d0
+        V(nx) = 1.0d0
+
+        ! set initial conditions of V
+        do i = 1, nx
+            V(i) = 0.0d0
+        end do
+        
     end subroutine
 
 end module variables_module
