80353f7aa49516a9939f405dfeca9501ff43000a
syoukera <s.akira2986@gmail.com>
2024-08-28 14:58:56 +0900

    [main] correct calculation of rho

--- main.f90
+++ main.f90
@@ -12,7 +12,7 @@ program main
 
         ! calclate density of electric charge
         do i = 1, nx
-            rho(i) = (n_pos(i) - n_neg(i) - n_neg(i))*q_e
+            rho(i) = (n_pos(i) - n_neg(i) - n_ele(i))*q_e
         end do
 
         call solve_poisson_equation()
@@ -37,11 +37,20 @@ program main
         print *, 'Did not converge after ', max_iter, ' iterations.'
     end if
 
+    ! calculate current density
+    do i = 2, nx-1
+
+        current_density(i) = (D_pos*((n_pos(i+1)-n_pos(i-1))/(2.0*dx)) - K_pos*n_pos(i)*E(i))*(+q_e) &
+                           + (D_pos*((n_neg(i+1)-n_pos(i-1))/(2.0*dx)) + K_neg*n_neg(i)*E(i))*(-q_e) &
+                           + (D_ele*((n_ele(i+1)-n_pos(i-1))/(2.0*dx)) + K_ele*n_ele(i)*E(i))*(-q_e)
+
+    end do
+
     ! output
     open(unit=1, file='potential_1d.dat', status='replace')
-    write(1,*) "X[m] rho[C/m3] V[V] E[V/m] n_pos[ions/m3] n_neg[ions/m3] n_ele[ions/m3]"
+    write(1,*) "X[m] rho[C/m3] V[V] E[V/m] n_pos[ions/m3] n_neg[ions/m3] n_ele[ions/m3] j[A/m2]"
     do i = 1, nx
-        write(1,*) i*dx, rho(i), V(i), E(i), n_pos(i), n_neg(i), n_ele(i)
+        write(1,*) i*dx, rho(i), V(i), E(i), n_pos(i), n_neg(i), n_ele(i), current_density(i)
     end do
     close(1)
 
