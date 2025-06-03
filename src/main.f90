24c532a26d6b3c5855b03d0f0b454ff0bbc4745d
syoukera <s.akira2986@gmail.com>
2025-06-03 15:15:31 +0900

    [main] comment out load_flame_height

--- src/main.f90
+++ src/main.f90
@@ -6,7 +6,7 @@ program main
     call initialize_variables()
     ! call import_variables('output/m300V/omega_V0.5_omega_pos0.05/potential_1d_100000.dat')
     
-    call load_flame_height()
+    ! call load_flame_height()
 
     call calculate_reaction_profile()
 
