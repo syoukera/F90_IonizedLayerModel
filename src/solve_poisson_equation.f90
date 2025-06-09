8a0dff129b5f3518de709438731ebc8f60fbc923
syoukera <s.akira2986@gmail.com>
2025-06-09 14:57:44 +0900

    [poisson_equation] apply noiman boundary on r = 0, R

--- src/solve_poisson_equation.f90
+++ src/solve_poisson_equation.f90
@@ -28,17 +28,19 @@ subroutine solve_poisson_equation
     end do
 
     ! center axis (i = 1) 
-    ! dV/dr = 0.0
-    do j = 2, nz-1
-        V(1, j) = (1/6.0D0)*(V(1, j+1) + 4.0D0*V(2, j) + V(1, j-1))
-    end do
+    V(1, :) = V(2, :) ! (noiman boundary)
+    ! ! Takuma (3.20)
+    ! do j = 2, nz-1
+    !     V(1, j) = (1/6.0D0)*(V(1, j+1) + 4.0D0*V(2, j) + V(1, j-1))
+    ! end do
 
     ! outlet (i = nr)
-    ! dV/dr = 0.0
-    do j = 2, nz-1
-        V(nr, j) = (1/4.0D0)*(V(nr, j+1) + 2.0D0*V(nr-1, j) + V(nr, j-1))
-        ! V(nr, j) = V_end/10
-    end do
+    V(nr, :) = V(nr-1, :) ! (noiman boundary)
+    ! ! Takuma (3.20)
+    ! do j = 2, nz-1
+    !     V(nr, j) = (1/4.0D0)*(V(nr, j+1) + 2.0D0*V(nr-1, j) + V(nr, j-1))
+    !     ! V(nr, j) = V_end/10
+    ! end do
 
     ! calclate error for check convergence
     error = error + maxval(abs(V - V_old))
