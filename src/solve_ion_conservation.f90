0d9744a80a4ca0ac7e027ee9c00dda4023af5535
syoukera <s.akira2986@gmail.com>
2025-06-24 11:22:41 +0900

    [ion_conservation] update upwind difference

--- src/solve_ion_conservation.f90
+++ src/solve_ion_conservation.f90
@@ -9,9 +9,9 @@ subroutine solve_ion_conservation(n_ion, n_ion_old, K_ion, Z_ion, D_ion, Sp_ion,
     integer :: i, j
     ! double precision :: n_ion_old (nr, nz)
     double precision :: a, bi, bj, ci, cj, d ! coefficients of discretised eq.
-    double precision :: ddVdr ! 2nd derivetive of voltage
+    double precision :: ddVdr, ddVdz ! 2nd derivetive of voltage
     double precision :: r_p, r_e, r_w ! r, z distance on center and mean value for North, South, West, East
-    double precision :: E_r_e, E_r_w, E_z_n, E_z_s ! mean value of E
+    double precision :: E_r_e, E_r_w, E_z_n, E_z_s, E_r_p ! mean value of E
 
     double precision, intent(inout) :: n_ion(nr, nz)
     double precision, intent(inout) :: n_ion_old(nr, nz)
@@ -37,8 +37,12 @@ subroutine solve_ion_conservation(n_ion, n_ion_old, K_ion, Z_ion, D_ion, Sp_ion,
             ! prepare mean value of E
             E_r_e = - (V(i+1, j) - V(i  , j))/dr
             E_r_w = - (V(i  , j) - V(i-1, j))/dr
+            E_r_p = (E_r_e + E_r_w)/2.0
             E_z_n = - (V(i, j+1) - V(i, j  ))/dz
             E_z_s = - (V(i, j  ) - V(i, j-1))/dz
+
+            ddVdr = (V(i+1, j) - 2.0*V(i, j) + V(i-1, j))/(dr**2)
+            ddVdz = (V(i, j+1) - 2.0*V(i, j) + V(i, j-1))/(dz**2)
 
             ! central difference for diffusion and source term
             a = 2.0*D_ion(i, j)/(dz**2) + D_ion(i, j)/(r_p*dr**2)*(r_e + r_w) - Sp_ion(i, j)
@@ -48,12 +52,15 @@ subroutine solve_ion_conservation(n_ion, n_ion_old, K_ion, Z_ion, D_ion, Sp_ion,
             cj = D_ion(i, j)/(dz**2)
 
             ! upwind difference for convection term r direction
-            a = a + (K_ion/(r_p*dr)) * (r_e*max(Z_ion*E_r_e, 0.0) - r_w*min(Z_ion*E_r_w, 0.0))
+            a = a + (K_ion/(r_p*dr)) * (r_e*max(Z_ion*E_r_e, 0.0) - r_w*min(Z_ion*E_r_w, 0.0)) &
+              - K_ion*ddVdr + (K_ion/r_p)*Z_ion*E_r_p
+            !   - K_ion*ddVdr + (K_ion/r_p)*Z_ion*E_r_p
             bi = bi - (K_ion/(r_p*dr)) * r_e * min(Z_ion*E_r_e, 0.0)
             ci = ci + (K_ion/(r_p*dr)) * r_w * max(Z_ion*E_r_w, 0.0)
 
             ! upwind difference for convection term z direction
-            a = a  + (K_ion/dz) * (max(Z_ion*E_z_n, 0.0) - min(Z_ion*E_z_s, 0.0))
+            a = a  + (K_ion/dz) * (max(Z_ion*E_z_n, 0.0) - min(Z_ion*E_z_s, 0.0)) &
+              - K_ion*ddVdz
             bj = bj - (K_ion/dz) * min(Z_ion*E_z_n, 0.0)
             cj = cj + (K_ion/dz) * max(Z_ion*E_z_s, 0.0)
 
